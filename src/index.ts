1ab24b1d14f3ca8a3e349ebf8a9541db6d97942b
tushartewari1303 <tushartewari1303@gmail.com>
2023-08-20 17:48:03 +0530

    backend ecommerce

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import { productRouter } from './routers/productRouter';
+import { seedRouter } from './routers/seedRouter';
+import { userRouter } from './routers/userRouter';
+
+dotenv.config();
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/tsmernamazonadb';
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch(() => {
+    console.log('error mongodb');
+  });
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/seed', seedRouter);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
